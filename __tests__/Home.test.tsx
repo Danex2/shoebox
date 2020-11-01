aef4e7bb78d13f01e75f2cfdb56308e43b019bfc
Danex2 <khadane.miller@gmail.com>
2020-11-01 14:30:29 -0500

    chore: add pre-commit hooks and add folder structure for tests

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,11 @@
+import { idText } from "typescript";
+import Home from "../pages/index";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("Testing the app", () => {
+  it("Should render the home page", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("Hello Shoebox!")).toBeInTheDocument();
+  });
+});
